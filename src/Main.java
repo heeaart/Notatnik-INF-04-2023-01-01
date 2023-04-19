bfd18798a7c87cf0ea305ccbcb3758f89c4295d5
szymon.sz <szymon366@vp.pl>
2023-04-19 14:10:59 +0200

    Info 0.4  2023

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String tytul1 = "Zrob kartkowke";
+        String tresc1 = "Pamietaj zrobic kartkowke 1gr 3tp";
+        String tytul2 = "nie sluchac maniaka";
+        String tresc2 = "Pamietaj nie sluchac mnie i maniaka";
+
+        Notatnik notatka1 = new Notatnik(tytul1, tresc1);
+        Notatnik notatka2 = new Notatnik(tytul2, tresc2);
+
+        notatka1.wyswietl();
+        notatka2.wyswietl();
+        notatka1.diagnozuj();
+        notatka2.diagnozuj();
     }
 }
--- src/Notatnik.java
+++ src/Notatnik.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Notatnik {
+/*
+***************************************
+*   Klasa: Notatka
+*   Opis: klasa prezentujaca notatke
+*   Pola: liczba notatek - statyczne pole przechowujace liczbe utworzonych notatek
+*           id - identyfikator notatki
+*           tytul - tytul notatki (tekst)
+*           tresc - tresc notatki (tekst)
+*   Autor: Pan alek
+***************************************
+ */
+public class Notatnik {
+    private static int LiczbaNotatek = 0;
+    private int id;
+    protected String tytul;
+    protected String tresc;
+
+    public void jeden(){
+
+    }
+
+    public void wyswietl(){
+        System.out.println(tytul + "\n" + tresc);
+    }
+
+    public void diagnozuj(){
+        System.out.println(LiczbaNotatek+";"+id+";"+tytul+";"+tresc);
+    }
+
+    public Notatnik(String tytul, String tresc) {
+        LiczbaNotatek++;
+        id = LiczbaNotatek;
+        this.tytul = tytul;
+        this.tresc = tresc;
+    }
 }
